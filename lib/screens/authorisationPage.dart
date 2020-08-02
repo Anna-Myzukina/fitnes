d11fa910465273670b8be49ebe5790b49fcb3664
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-08-02 07:33:23 +0300

    Change color of inputs

--- lib/screens/authorisationPage.dart
+++ lib/screens/authorisationPage.dart
@@ -6,6 +6,8 @@ class AuthorisationPage extends StatefulWidget {
 }
 
 class _AuthorisationPageState extends State<AuthorisationPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     Widget _logo() {
@@ -25,36 +27,74 @@ class _AuthorisationPageState extends State<AuthorisationPage> {
       );
     }
 
-    Widget _form(String label, void func()){
+    Widget _imput(Icon icon, String hint, TextEditingController controller,
+        bool obsecure) {
       return Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 20, top: 10),
-              child: Text('Email'),
-              ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 20),
-              child: Text('Password'),
-              ),
-            SizedBox(height: 20,),
-            Padding(
-              padding: EdgeInsets.only(left: 20, right:  20),
-              child: Container(
-                height: 50,
-                width: MediaQuery,
-              ),)
-           ],
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: Container(
+          color: Colors.blueGrey[300],
+        child: TextField(
+          controller: controller,
+          obscureText: obsecure,
+          style: TextStyle(fontSize: 20, color: Colors.white),
+          decoration: InputDecoration(
+              hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white30),
+              hintText: hint,
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 3)),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white54, width: 1)),
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                child: IconTheme(
+                  data: IconThemeData(color: Colors.white),
+                  child: icon,
+                ),
+              )
+          ),
+        ),
+        ),
+      );
+    }
+
+    Widget _form(String label, void func()) {
+      return Container(
+        
+       child: Column(    
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 20, top: 10),
+            child: _imput(Icon(Icons.email), "EMAIL", _emailController, false),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child:
+                _imput(Icon(Icons.lock), "PASSWORD", _passwordController, true),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              child: Text(label),
+            ),
           )
-      )
+        ],
+      ));
     }
 
     return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
+        backgroundColor: Colors.blueGrey,
         body: Column(
           children: <Widget>[
             _logo(),
-            _form(),
+            _form('LOGIN', () {}),
           ],
         ));
   }

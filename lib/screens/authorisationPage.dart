5d5d7440b8959e256b67ebf1968ea7eb502c22bf
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-08-02 07:13:50 +0300

    Add email and passwor hints

---
+++ lib/screens/authorisationPage.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class AuthorisationPage extends StatefulWidget {
+  @override
+  _AuthorisationPageState createState() => _AuthorisationPageState();
+}
+
+class _AuthorisationPageState extends State<AuthorisationPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    Widget _logo() {
+      return Padding(
+        padding: EdgeInsets.only(top: 100),
+        child: Container(
+          child: Align(
+            child: Text(
+              'FitTrak',
+              style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget _imput(Icon icon, String hint, TextEditingController controller,
+        bool obsecure) {
+      return Container(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: TextField(
+          controller: controller,
+          obscureText: obsecure,
+          style: TextStyle(fontSize: 20, color: Colors.white),
+          decoration: InputDecoration(
+              hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.white30),
+              hintText: hint,
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white, width: 3)),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white54, width: 1)),
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                child: IconTheme(
+                  data: IconThemeData(color: Colors.white),
+                  child: icon,
+                ),
+              )
+          ),
+        ),
+      );
+    }
+
+    Widget _form(String label, void func()) {
+      return Container(
+          child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 20, top: 10),
+            child: _imput(Icon(Icons.email), "EMAIL", _emailController, false),
+          ),
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child:
+                _imput(Icon(Icons.lock), "PASSWORD", _passwordController, true),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              child: Text(label),
+            ),
+          )
+        ],
+      ));
+    }
+
+    return Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: Column(
+          children: <Widget>[
+            _logo(),
+            _form('LOGIN', () {}),
+          ],
+        ));
+  }
+}
